152f2209322ed54a6dc1e7b1e2d928c52b9195ff
Jeremy Che <jeremy.che48@gmail.com>
2018-01-28 09:57:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  // for(int i = 0; i < 25; i++) {
+  // for(var i = 0; i < 25; i++) {
   //   if(i+1 !== 1) {
   //     tempString = `I am ${i+1} strange loops.`
   //   } else {
